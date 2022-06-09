e2e3af1d9c3a86769fa0cbcb521ceb8564ef86cd
Nazirul Fikri <106502768+alfiieeee@users.noreply.github.com>
2022-06-09 12:54:53 +0800

    Update coding.py

--- coding.py
+++ coding.py
@@ -4,6 +4,15 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
+latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+   
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+        
 st.write("""
 # Weather Prediction in Seattle
 This app predicts the **Weather** type!
